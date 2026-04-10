c831d5991502ae50d8cbb390ed7d219a1f5b89e7
kkm121 <787muthu@gmail.com>
2026-04-10 08:40:48 +0530

    Added UC7 files

---
+++ src/com/srm/helloapp/uc7helloapp.java
@@ -0,0 +1,12 @@
+package com.srm.helloapp;
+
+public class uc7helloapp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
